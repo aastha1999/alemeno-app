a5e22510c644284ebf25eb92b755838dccc9f24b
Aastha <asthana.aastha1999@gmail.com>
2022-03-02 22:46:15 +0530

    add allowed hosts

--- alemenoapp/settings.py
+++ alemenoapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','alemeno-django-app.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','alemeno-app.herokuapp.com/']
 
 
 # Application definition
